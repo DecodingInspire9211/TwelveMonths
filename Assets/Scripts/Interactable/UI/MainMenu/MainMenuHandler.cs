f070747faad3f80fad9790d5b3215aa35fa47abd
DecodingInspire9211 <beierkw9211@gmail.com>
2022-10-11 18:58:45 +0200

    optimisation

--- Assets/Scripts/Interactable/UI/MainMenu/MainMenuHandler.cs
+++ Assets/Scripts/Interactable/UI/MainMenu/MainMenuHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,17 +13,47 @@ public class MainMenuHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        progressBar = GameObject.Find("progressLoader").GetComponent<Slider>();
+        if(progressBar)
+        {
+            Debug.Log(progressBar.name);
+        }   
+        else
+        {
+            Debug.Log("No object found");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        progressBar.value = Mathf.Clamp01(loadingScene.progress / 0.9f);
+        if(loadingScene == null)
+            progressBar.value = Mathf.Clamp01(0f / 0.9f);
+
+        if(loadingScene != null)
+            progressBar.value = Mathf.Clamp01(loadingScene.progress / 0.9f);
     }
 
     public void NewGame()
     {
-        loadingScene = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        try
+        {
+            loadingScene = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex);
+        }
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
     }
 }
